3ae0ab0ac6e0292160b9ab4e8d1fd9783b61f82a
matthew-s-white <white753@purdue.edu>
2022-10-30 18:01:28 -0400

    setup of backend and frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css';
+import LognInPage from './routes/log-in-page';
+import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {  
+    path: "/",
+    element: <LognInPage />,
+  }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ frontend/src/routes/log-in-page.js
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+import './log-in-page.css';
+
+function LogInPage() {
+
+    return (
+    <div className='log_in_page_container'>
+
+    </div>
+    );
+}
+export default LogInPage;
