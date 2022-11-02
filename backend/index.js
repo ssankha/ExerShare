69987567a396ac21fc83918796b4f88b702d6cc5
Oliver Miller <miller.oliver@icloud.com>
2022-11-01 20:45:39 -0400

    contains stuff changed right before demo

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Added by oliver, this route will actually work and call the DB. Will look at the `Users` table.
-app.route('/api/user/:searchUserEmail')
-    .get(function(req, res, next) {
+
+app.route('/api/user/:searchUserEmail').get(function(req, res, next) {
     connection.query(
         "SELECT userId, email, bio FROM `Users` WHERE email = ?", req.params.searchUserEmail,
         function(error, results, fields) {
@@ -23,20 +22,53 @@ app.route('/api/user/:searchUserEmail')
     );
 });
 
-app.post('/api/signIn', function(req, res){
+app.route('/api/signIn').post(function(req, res, next) {
+    console.log(`\tUsername: ${req.body.email}\n\tPassword: ${req.body.password}`);
+    connection.query(
+        "SELECT * FROM `Users` WHERE email = ? AND password = ?", [req.body.email, req.body.password],
+        function(error, results, fields) {
+            if (error) throw error;
+            else if (results.length > 0) {
+                console.log("Logged in.");
+                console.log(results);
+                res.json({ "status": "success" });
+            }
+            else {
+                console.log("Incorrect username or password.");
+                res.json({ "status": "failure" });
+            }
+        }
+    );
+});
 
-    // line below to be replaced by sql query
+// app.post('/api/signIn', function(req, res){
+
+//     // line below to be replaced by sql query
     
-    if(req.body.email === "blah" && req.body.password === "1234"){
-        res.json({ status: "success" });
-    } else {
-        res.json({status: "failed"})
-    }
-  });
+//     if(req.body.email === "blah" && req.body.password === "1234"){
+//         res.json({ status: "success" });
+//     } else {
+//         res.json({status: "failed"})
+//     }
+//   });
 
 app.post('/api/register', function(req, res){
 
-    // line below to be replaced by sql query
+    console.log(`\tUsername: ${req.body.email}\n\tPassword: ${req.body.password}`);
+    connection.query(
+        "INSERT INTO `Users` (email, password) VALUES (?, ?)", [req.body.email, req.body.password],
+        function(error, results, fields) {
+            if (error) {
+                console.log("Error registering.");
+                throw error;
+            }
+            else {
+                console.log("Registered.");
+                console.log(results);
+                res.json({ "status": "success" });
+            }
+        }
+    );
 
     if(req.body.email === "blah" && req.body.password === "1234"){
         res.json({ status: "success" });
@@ -45,6 +77,7 @@ app.post('/api/register', function(req, res){
     }
 });
 
+// TODO: later
 app.post('/api/post', function(req, res){
 
     // add SQL query that adds post to database
